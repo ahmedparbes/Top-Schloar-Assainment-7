7f875c00833e5fab9e3ef01dfe91509612d0f729
ahmedparbes <fbhelpsbd@gmail.com>
2021-09-26 18:58:17 +0600

    Added

--- src/Components/Total/Total.js
+++ src/Components/Total/Total.js
@@ -8,13 +8,13 @@ const Total = (props) => {
     let image = ''
     for (const scholar of counts) {
         total = total + scholar.salary;
-        name = name + scholar.name + ' ';
+        name = name + scholar.name + '  ';
         image = image + scholar.img;
 
     }
     return (
         <div>
-            <h2>Scholar Added : {props.count.length} </h2>
+            <h2>Scholars Added : {props.count.length} </h2>
             <h4>Total Salary : {total} </h4>
             <div id="set-name-img">
                 <img src={image} alt="" />
